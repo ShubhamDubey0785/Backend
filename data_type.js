4042cd8637906b14bc71960bee9e53737ba72b8d
Shubham Dubey <shubhamdubey0785@gmail.com>
2024-02-17 22:51:38 +0530

    Initial commit

---
+++ data_type.js
@@ -0,0 +1,51 @@
+/**
+ * Primitive
+ */
+
+//String
+/**
+var name="Rahul";
+
+var title ='Singh'
+
+console.log(name)
+
+console.log(typeof name)
+
+console.log(typeof(name))**/
+
+//Number
+/**
+var age=18;
+var x=1.4;
+
+console.log(typeof x);
+
+// BigInt 
+var big_num=123n;
+console.log(typeof big_num);
+
+console.log(typeof NaN);*/
+
+
+//null
+
+// console.log(typeof null); //object
+
+// var name
+// console.log(typeof name)
+
+
+// console.log(typeof true)
+
+// Sysbol
+// var s1=Symbol()  //constructor  // symbols object
+
+
+//Objects
+var person={
+    name:"Rahul",
+    age:18,
+    hobby:"Cricket"
+}
+console.log(person)
