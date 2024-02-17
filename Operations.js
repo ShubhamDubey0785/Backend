31805a705ed3ec224d61e1d3d77849428533c12e
Shubham Dubey <shubhamdubey0785@gmail.com>
2024-02-17 22:52:25 +0530

    Initial commit

---
+++ Operations.js
@@ -0,0 +1,59 @@
+/**
+ * Assignment operators
+ */
+
+var name="Rahul";
+
+// Arithmatic operators
+// console.log(5+6);
+// console.log(5*6);
+// console.log(2**5);
+// console.log(122213124%17);
+
+/**
+var i=1;
+i=i+1;
+console.log(i);**/
+
+// var p=5
+// console.log(p++);
+// console.log(p);
+
+// console.log(++p);
+// console.log(p);
+
+
+// Relational operator
+
+// Equality
+// console.log(1==3);
+
+// console.log(1=='1')// internally convert and then compare the value
+
+// Strict equality check
+// console.log(1==='1')
+
+/**
+ * chech the inequality
+ */
+// console.log(1!='1')
+
+// console.log(1!=='1')
+
+/**
+ * Comparative
+ */
+// console.log(3>=4)
+
+// Logical operators
+// console.log(3>4 && true)
+
+// console.log(3>4 || true)
+
+// console.log(true ^ true)//X-OR
+// console.log(true ^ false)//X-OR
+
+// Bitwise Operator
+console.log(10 & 6)
+console.log(10 | 6)
+
