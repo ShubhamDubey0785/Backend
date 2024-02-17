b96a1cc85ff98b25f6f7330627c7846848e9d15e
Shubham Dubey <shubhamdubey0785@gmail.com>
2024-02-17 22:30:18 +0530

    initial commit

---
+++ conditionals.js
@@ -0,0 +1,51 @@
+// var num=21;
+
+// if(num%2==0){
+//     console.log("Even");
+// }else{
+//     console.log("Odd");
+// }
+
+// console.log("After if");
+
+
+// // terary operator -> if-else
+// num%2==0 ? console.log("Even") : console.log("Odd");
+
+
+/**
+ * Nested conditionals
+ */
+
+// var a=10
+
+// if(a>=0){
+//     if(a>5){
+//         console.log("a is greater than zero");
+//     }else{
+//         console.log("a is greater than zero but less than 5");
+//     }
+// }else{
+//     console.log("a js less than zero");
+// }
+
+// a>0 ? a>5 ?console.log("a is greater than 0 and 5")
+// : console.log("A is greater than 0 but less than 5"): console.log("a is less than 0")
+
+
+
+/**
+ * Switch
+ */
+var fruit="Apple"
+
+switch(fruit){
+    case "Apple":
+        console.log("It is an apple")
+        break
+    case "Apple":
+        console.log("It is a apple")
+        break;
+    default:
+        console.log("Not a valid fruit")
+}
